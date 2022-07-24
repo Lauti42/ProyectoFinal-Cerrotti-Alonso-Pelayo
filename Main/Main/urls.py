983109695f17efa3112be5aac79d73414b85fedb
Lautaro <lautarocerrotti@gmail.com>
2022-07-24 19:02:16 -0300

    Se agregan las url

--- Main/Main/urls.py
+++ Main/Main/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+from django.urls import include, path
+from ProyectoFinal.views import aboutview, contactview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('RegistroUsuarios.urls')),
+    path('registrado/', include('RegistroUsuarios.urls')),
+    path('registrarse/', include('RegistroUsuarios.urls')),
+    path('about/', aboutview),
+    path('contact/', contactview),
+    
 ]
