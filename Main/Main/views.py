aa929585609796f95ac5eaeedff16b6b215b1a04
Lautaro <lautarocerrotti@gmail.com>
2022-07-24 20:50:24 -0300

    Se agrega un condicional a la funcion registro RegistroUsuario.views

--- Main/Main/views.py
+++ Main/Main/views.py
@@ -13,3 +13,4 @@ def contactview(request):
     
 
         
+        
--- Main/RegistroUsuarios/views.py
+++ Main/RegistroUsuarios/views.py
@@ -11,18 +11,18 @@ def registrarse(request):
     return render(request, 'registrarse.html')
 
 def registro(request):
-    
-    print("POST")
+    if request.method == 'POST':
+        print("POST")
 
-    #Obteniendo datos del registro (Form)
-    nombre = request.POST['Usuario']
-    email = request.POST['Email']
-    password = request.POST['Contraseña']
-    password2 = request.POST['Contraseña2']
+        #Obteniendo datos del registro (Form)
+        nombre = request.POST['Usuario']
+        email = request.POST['Email']
+        password = request.POST['Contraseña']
+        password2 = request.POST['Contraseña2']
 
-    #Guardando los datos en la DB
-    User_registred = Registro_usuarios(nombre=nombre, email=email, password=password)
-    User_registred.save()
-        
-    documentoDeTexto = f"Integrante creado con exito: {nombre} {email} {password} {password2}"
-    return render(request, "indexregistrado.html", {'documentoDeTexto': documentoDeTexto})
+        #Guardando los datos en la DB
+        User_registred = Registro_usuarios(nombre=nombre, email=email, password=password)
+        User_registred.save()
+            
+        documentoDeTexto = f"Integrante creado con exito: {nombre} {email} {password} {password2}"
+        return render(request, "indexregistrado.html", {'documentoDeTexto': documentoDeTexto})
