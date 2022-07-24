74f8fc76cf6ca0a8d7367ffe9cd718d4abde2475
Lautaro <lautarocerrotti@gmail.com>
2022-07-24 19:10:47 -0300

    Se agrega una funcion para obtener atravez de post los datos ingresados

--- Main/RegistroUsuarios/views.py
+++ Main/RegistroUsuarios/views.py
@@ -10,3 +10,19 @@ def indexview(request):
 def registrarse(request):
     return render(request, 'registrarse.html')
 
+def registro(request):
+    
+    print("POST")
+
+    #Obteniendo datos del registro (Form)
+    nombre = request.POST['Usuario']
+    email = request.POST['Email']
+    password = request.POST['Contraseña']
+    password2 = request.POST['Contraseña2']
+
+    #Guardando los datos en la DB
+    User_registred = Registro_usuarios(nombre=nombre, email=email, password=password)
+    User_registred.save()
+        
+    documentoDeTexto = f"Integrante creado con exito: {nombre} {email} {password} {password2}"
+    return render(request, "indexregistrado.html", {'documentoDeTexto': documentoDeTexto})
