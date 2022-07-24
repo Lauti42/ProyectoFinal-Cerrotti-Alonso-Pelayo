8fb2861ec2afe159ceccc21fed4b4d70311a38bd
Lautaro <lautarocerrotti@gmail.com>
2022-07-24 19:00:53 -0300

    Se agregan static

--- Main/Main/settings.py
+++ Main/Main/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+TATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+LOGIN_REDIRECT_URL = 'index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
