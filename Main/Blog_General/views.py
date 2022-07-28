460d96e3dc63a2dd020bdbac2afd94b72f056443
laupelayo <laureano_pelayo@hotmail.com>
2022-07-28 14:17:56 -0300

    Se agrego la view GeneralPost y se modifico html

--- Main/Blog_General/views.py
+++ Main/Blog_General/views.py
@@ -26,30 +26,8 @@ def NewPost(request):
     return render(request, 'makeanewpost.html')
 
 
-def GeneralPost(request):
+def GeneralPost(self):
 
-    nombre= None
-    contenido= None
-    imagen= None
-    autor=  None
-    fecha=  None
+    post= Entry.objects.all()
 
-    # for entry in Entry.objects.all():
-    #     for post in entry:
-    #         nombre= post.nombre
-    #         contenido= post.contenido
-    #         imagen= post.imagen
-    #         autor= post.autor
-    #         fecha= post.fecha
-
-    
-    entries = Entry.objects.all()
-    for entry in entries:
-        nombre = entry.nombre
-        contenido = entry.contenido
-        imagen = entry.imagen
-        autor = entry.autor
-        fecha = entry.fecha
-        
-    #return render(request, "Blog_Generalindex.html", {'entries': entries, 'nombre': nombre, 'contenido': contenido, 'imagen': imagen, 'autor': autor, 'fecha': fecha})
-    return render(request, "Blog_Generalindex.html", {'entries': entries, 'nombre': nombre, 'contenido': contenido, 'imagen': imagen, 'autor': autor, 'fecha': fecha})
+    return render(self, 'Blog_Generalindex.html', {'post': post})
