08450a92e79ef4b9bbc267bede602057d5ef0819
laupelayo <laureano_pelayo@hotmail.com>
2022-07-30 00:05:23 -0300

    ahora en blog home aparecen todos los posteos y en home los que elegimos

--- Main/Main/views.py
+++ Main/Main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Blog_General.models import Entry
 
-def indexview(request):
-    return render(request, 'indexBase.html')
+def indexview(self):
+
+    posteos = Entry.objects.filter(muestra_inferior= 'si')
+
+    return render(self, 'indexBase.html', {'posteos': posteos} )
 
 def aboutview(request):
     return render(request, 'about.html')
