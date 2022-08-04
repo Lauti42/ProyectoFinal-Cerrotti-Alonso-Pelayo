1ee16c4549221d1ad3d58f0c2051c1e466c21873
laupelayo <laureano_pelayo@hotmail.com>
2022-08-04 00:11:58 -0300

    Se agrega login

--- Main/RegistroUsuarios/urls.py
+++ Main/RegistroUsuarios/urls.py
@@ -1,7 +1,7 @@
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import include, path 
-from RegistroUsuarios.views import registro , registrarse, preferencias
+from RegistroUsuarios.views import registro , registrarse, preferencias, login_request
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('registrado/', registro,  name='registrado'),
     path('registrarse/', registrarse, name='registrarse'),
     path('preferencias_enviadas/', preferencias, name='preferencias'),
+    path('login', login_request, name= 'login')
 ]
