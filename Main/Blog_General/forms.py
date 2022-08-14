61cd479b92d73cb7dc7e80187ce55afb0c0d1667
laupelayo <laureano_pelayo@hotmail.com>
2022-08-14 20:05:14 -0300

    Se agrega diseño en formulario de comentario

--- Main/Blog_General/forms.py
+++ Main/Blog_General/forms.py
@@ -1,9 +1,18 @@
 from socket import fromshare
 from django import forms
 from .models import Entry, Comentario
+from django.db import models
 
 class NewCommentForm(forms.ModelForm):
     class Meta:
         model= Comentario
         fields= ("body",)
+        widgets= {
+            'body': forms.TextInput(
+                attrs={
+                    'placeholder': 'Comenta aqui'
+                }
+            )
+        }
+        
         
