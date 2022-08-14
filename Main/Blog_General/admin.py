291899722601c69f6ed8b46b8425fc12e25b492d
laupelayo <laureano_pelayo@hotmail.com>
2022-08-13 23:54:05 -0300

    se agrega comentario

--- Main/Blog_General/admin.py
+++ Main/Blog_General/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from Blog_General.models import Entry
+from Blog_General.models import Entry, Comentario
 
 # Register your models here.
 
 admin.site.register(Entry)
+admin.site.register(Comentario)
