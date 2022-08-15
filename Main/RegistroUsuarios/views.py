b86426d606f4973e233faa5ee841e668c6bb72a5
Lautaro <lautarocerrotti@gmail.com>
2022-08-15 17:42:26 -0300

    Se da formato al formulario de modificar perfil, falta agregar Mi perfil al lado , actualizar css

--- Main/RegistroUsuarios/views.py
+++ Main/RegistroUsuarios/views.py
@@ -33,18 +33,6 @@ def registro(request):
     return render(request, 'registrarse.html', {'formreg': formreg, 'form': form})    
 
 
-        #Obteniendo datos del registro (Form)
-        #nombre = request.POST['Usuario']
-        #email = request.POST['Email']
-        #password = request.POST['Contraseña']
-        #password2 = request.POST['Contraseña2']
-    
-        #Guardando los datos en la DB
-        #User_registred = Registro_usuarios(nombre=nombre, email=email, password=password)
-        #User_registred.save()
-            
-        #documentoDeTexto = f"Integrante creado con exito: {nombre} {email} {password} {password2}"
-        #return render(request, "indexregistrado.html", {'documentoDeTexto': documentoDeTexto})
 
 def preferencias(request):
     if request.method == "POST":
